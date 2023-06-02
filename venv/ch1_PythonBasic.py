1645e55be7d8f6427eb87e0c4dc51b40eda2053d
gariGG <ggeorgiev378@gmail.com>
2023-06-02 17:52:49 +0300

    first test commit

---
+++ venv/ch1_PythonBasic.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+# Running Python
+6000 + 4523.50 + 134.25
+_ + 8192.75
+
+
+def fun(name: str):
+    if isinstance(name, str):
+        print(name)
+    else:
+        print('Missing String!')
+
+
+# 1.2 Python Programs
+
+
+
+
